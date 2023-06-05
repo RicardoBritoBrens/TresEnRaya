ad08014f514e48990691db073e13802e5169df4f
Ricardo Brito <ricardo_brito@bhdleon.com.do>
2023-06-05 08:10:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let showSelectPlayers = true;
+let showSelectPlayerIcon = false;
+let showBoard = false;
+
+function enableSelectPlayers(){
+    this.showSelectPlayers = false;
+}
+
+function disableSelectPlayers(){
+    this.showSelectPlayers = false;
+}
+
+
+function enableSelectIcon(){
+    this.showSelectPlayerIcon = true;
+}
+
+function disableSelectIcon(){
+    this.showSelectPlayerIcon = false;
+}
+
+
+function enableBoard(){
+    this.showBoard = true;
+}
+
+function disableBoard(){
+    this.showBoard = false;
+}
