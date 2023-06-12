e58c4be8a8b5f71aa2cfee3dc238fb94ceabae3a
Ricardo Brito <ricardo_brito@bhdleon.com.do>
2023-06-11 23:52:16 -0400

    [ADD] winner board functionality only for rows

--- index.js
+++ index.js
@@ -1,89 +1,157 @@
-let showSelectPlayers = true;
-let showSelectPlayerIcon = false;
-let showBoard = false;
+// Game Status
+let gameAlreadyStart = false;
+
+// Players
 let players = 0;
 let playerIcon = '';
-let gameAlreadyStart = true;
-let selectPlayersDivName = 'select-players-div';
-let selectPlayerIconDivName = 'select-player-icon-div';
-let boardDivName = 'board';
 
-function setVisibilityToDiv(divId, visibility){    
-    debugger;    
-    let currentDiv = this.document.getElementById(divId);     
-    if (currentDiv.style.display === "show") {
-        currentDiv.style.display = "block";
-      } else {
-        currentDiv.style.display = "none";
-      }
-}
+// Constants
+const selectPlayersDivName = 'select-players-div';
+const selectPlayerIconDivName = 'select-player-icon-div';
+const boardDivName = 'board-div';
+const boardInnerDivName = 'board-inner-div';
+const youWinDivName = 'you-win-div';
 
+// Board
+let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// Players functions
 function onePlayerVsIa(){    
     players = 1;
     playAudio('dragonzord-flute');
-    setVisibilityToDiv(selectPlayersDivName, 'none');
+    setVisibilityToDiv(selectPlayersDivName, 'hide');
     setVisibilityToDiv(selectPlayerIconDivName, 'show');
 }
-
 function twoPlayers(){    
     players = 2;
     playAudio('dragonzord-sound-effect');
-    setVisibilityToDiv(selectPlayersDivName, 'none');
+    setVisibilityToDiv(selectPlayersDivName, 'hide');
     setVisibilityToDiv(selectPlayerIconDivName, 'show');
 }
 
+// Select Icons functions
 function useIconX(){    
     playerIcon = 'x';
     playAudio('megaman-3-death-sound-effect');        
-    setVisibilityToDiv(selectPlayerIconDivName, 'none');
+    setVisibilityToDiv(selectPlayerIconDivName, 'hide');
     setVisibilityToDiv(boardDivName, 'show');
 }
-
 function useIconY(){    
     playerIcon = 'y';
     playAudio('mm4-victory');
-    setVisibilityToDiv(selectPlayerIconDivName, 'none');
+    setVisibilityToDiv(selectPlayerIconDivName, 'hide');
     setVisibilityToDiv(boardDivName, 'show');
 }
 
-function enableSelectPlayers(){
-    this.showSelectPlayers = false;
+// Functionalities browser functions
+function setVisibilityToDiv(divId, visibility)
+{   
+    let currentDiv = this.document.getElementById(divId);
+    if(visibility === "hide")
+    {
+        if (currentDiv.style.display === "show") 
+        {
+            currentDiv.style.display = "block";
+        } else {
+            currentDiv.style.display = "none";
+        }
+    }
+    else if(visibility === "show"){
+        currentDiv.style.display = "block";
+    }  
 }
-
-function disableSelectPlayers(){
-    this.showSelectPlayers = false;
-}
-
-
-function enableSelectIcon(){
-    this.showSelectPlayerIcon = true;
-}
-
-function disableSelectIcon(){
-    this.showSelectPlayerIcon = false;
-}
-
-
-function enableBoard(){
-    this.showBoard = true;
-}
-
-function disableBoard(){
-    this.showBoard = false;
-}
-
 
 function playAudio(audioName) {
     var audio = new Audio(`./assets/sounds/${audioName}.mp3`);
-    audio.play();
-  }
+    //audio.play();
+}
 
+function setClassToLink(index, iconClass){        
+    let innerDiv = document.getElementById('board-inner-div');    
+    let link = innerDiv.children[index];    
+    link.classList.add(iconClass);    
+}
 
-//   const onConfirmRefresh = function (event) {
-//     if(event.preventDefault())
-//     {
-//         return event.returnValue = "Are you sure you want to leave the page, the game is in progress?";
-//     }    
-//   }
-  
-//   window.addEventListener("beforeunload", onConfirmRefresh, { capture: true });
+function drawWinner(){
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
+    context.lineWidth = 20;    
+    context.beginPath();
+    context.moveTo(width, 100);
+    context.lineTo(10, 100);    
+    context.strokeStyle = 'green';    
+    context.stroke();
+}
+//drawWinner();
+
+drawFirstRowWinner();
+drawSecondRowWinner();
+drawThirdRowWinner();
+
+function drawFirstRowWinner(){
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
+    context.lineWidth = 20;    
+    setupWinnerRow(1, context, width, height);
+    context.strokeStyle = 'green';    
+    context.stroke();
+}
+
+function drawSecondRowWinner(){
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
+    context.lineWidth = 20;    
+    setupWinnerRow(2, context, width, height);
+    context.strokeStyle = 'green';    
+    context.stroke();
+}
+
+function drawThirdRowWinner(){
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
+    context.lineWidth = 20;    
+    setupWinnerRow(3, context, width, height);
+    context.strokeStyle = 'green';    
+    context.stroke();
+}
+
+function setupWinnerRow(number, context, width){    
+    switch(number) 
+    {
+        case 1:
+            context.beginPath();
+            context.moveTo(width, 100);
+            context.lineTo(0, 100);  
+          break;
+        case 2:
+            context.beginPath();
+            context.moveTo(width, width - 100);
+            context.lineTo(0, width - 100);
+          break;
+        case 3:
+            context.beginPath();
+            context.moveTo(0, 275);
+            context.lineTo(width, 275);
+          break;        
+      }      
+}
+// Testing assign icon
+// setClassToLink(0,'o');
+// setClassToLink(1,'x');
+// setClassToLink(2,'o');
+
+// setClassToLink(3,'x');
+// setClassToLink(4,'o');
+// setClassToLink(5,'x');
+
+// setClassToLink(6,'o');
+// setClassToLink(7,'x');
+// setClassToLink(8,'o');
